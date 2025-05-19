a6a765e55bd4a2a683627478041ef585631108c3
Francisco Junior <francisco.junior@ipdec.org>
2025-05-19 09:28:18 -0400

    improve: adicionado validação de pacientes as rotas destinadas aos pacientes

--- src/modulo_geral/controllers/paciente-consulta.controller.ts
+++ src/modulo_geral/controllers/paciente-consulta.controller.ts
@@ -1,4 +1,17 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post, Put, Query, Req, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Put,
+  Query,
+  Req,
+  UnauthorizedException,
+  UseGuards,
+} from '@nestjs/common';
 import { PacienteSaveDto } from '../dto/paciente.save.dto';
 import { PacienteService } from '../services/paciente.service';
 import { AuthGuard } from '../../guards/auth.guard';
@@ -7,6 +20,7 @@ import { TokenUserDto } from '../dto/token-user.dto';
 import { ConsultaService } from '../services/consulta.service';
 import { ConsultaSaveDto } from '../dto/consulta.save.dto';
 import { ConsultaUpdateDto } from '../dto/consulta.update.dto';
+import { TipoUsuarioEnum } from '../../common/enums/tipo.usuario.enum';
 
 @UseGuards(AuthGuard)
 @Controller("paciente/consulta")
@@ -17,30 +31,41 @@ export class PacienteConsultaController {
   @Get()
   async listarMarcadas(@Req() req: any, @Query('id') id: string) {
     const user:TokenUserDto = await this.authService.tokenDecode(req);
+    this.validarPaciente(user);
     return this.consultaService.listarMarcadas(user.cpf, id);
   }
 
   @Get('historico')
   async historico(@Req() req: any, @Query('id') id: string) {
     const user:TokenUserDto = await this.authService.tokenDecode(req);
+    this.validarPaciente(user);
     return this.consultaService.historico(user.cpf, id);
   }
 
   @Post()
   async marcar(@Req() req: any, @Body() body: ConsultaSaveDto) {
     const user:TokenUserDto = await this.authService.tokenDecode(req);
+    this.validarPaciente(user);
     return this.consultaService.marcar(user.cpf, body);
   }
 
   @Put(':id')
   async remarcar(@Req() req: any, @Body() body: ConsultaUpdateDto, @Param('id') id: string,) {
     const user:TokenUserDto = await this.authService.tokenDecode(req);
+    this.validarPaciente(user);
     return this.consultaService.remarcar(user.cpf, body, id);
   }
 
   @Patch('cancelar/:id')
   async cancelar(@Param('id') id: string, @Req() req: any) {
     const user:TokenUserDto = await this.authService.tokenDecode(req);
+    this.validarPaciente(user);
     return this.consultaService.cancelar(user.cpf, id);
+  }
+
+  private validarPaciente(user: TokenUserDto) {
+    if (user.tipo !== TipoUsuarioEnum.PACIENTE) {
+      throw new UnauthorizedException('Acesso não autorizado');
+    }
   }
 }
--- src/modulo_geral/controllers/paciente.controller.ts
+++ src/modulo_geral/controllers/paciente.controller.ts
@@ -1,9 +1,10 @@
-import { Body, Controller, Delete, Get, Param, Post, Put, Req, UseGuards } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Put, Req, UnauthorizedException, UseGuards } from '@nestjs/common';
 import { PacienteSaveDto } from '../dto/paciente.save.dto';
 import { PacienteService } from '../services/paciente.service';
 import { AuthGuard } from '../../guards/auth.guard';
 import { AuthService } from '../services/auth.service';
 import { TokenUserDto } from '../dto/token-user.dto';
+import { TipoUsuarioEnum } from '../../common/enums/tipo.usuario.enum';
 
 @UseGuards(AuthGuard)
 @Controller("paciente")
@@ -13,18 +14,27 @@ export class PacienteController {
   @Get()
   async buscar(@Req() req: any) {
     const user:TokenUserDto = await this.authService.tokenDecode(req);
+    this.validarPaciente(user);
     return this.service.findOneByCpf(user.cpf);
   }
 
   @Put()
   async atualizar(@Req() req: any, @Body() body: PacienteSaveDto) {
     const user:TokenUserDto = await this.authService.tokenDecode(req);
+    this.validarPaciente(user);
     return this.service.update(user.cpf, body);
   }
 
   @Delete()
   async apagar(@Req() req: any) {
     const user:TokenUserDto = await this.authService.tokenDecode(req);
+    this.validarPaciente(user);
     return this.service.delete(user.cpf);
+  }
+
+  private validarPaciente(user: TokenUserDto) {
+    if (user.tipo !== TipoUsuarioEnum.PACIENTE) {
+      throw new UnauthorizedException('Acesso não autorizado');
+    }
   }
 }
